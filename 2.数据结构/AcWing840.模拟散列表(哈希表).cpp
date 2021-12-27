725bd652d03f3452842caab999dc2abeab576812
030666 <2028800763@qq.com>
2021-12-27 22:32:33 +0800

    哈希表(拉链法)

---
+++ 2.数据结构/AcWing840.模拟散列表(哈希表).cpp
@@ -0,0 +1,53 @@
+// 时间：2021.12.27 22点10分 
+//	拉链法 
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+const int N = 100003;//取模的这个数N最好是离2^n尽可能的远的质数，这样出现哈希冲突的概率最小
+
+int h[N], e[N], ne[N], idx;
+
+void insert(int x)
+{
+    int k = (x % N + N) % N;
+    e[idx] = x;
+    ne[idx] = h[k];
+    h[k] = idx ++ ;
+}
+
+bool find(int x)
+{
+    int k = (x % N + N) % N;
+    for (int i = h[k]; i != -1; i = ne[i])
+        if (e[i] == x)
+            return true;
+
+    return false;
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    memset(h, -1, sizeof h);
+
+    while (n -- )
+    {
+        char op[2];
+        int x;
+        scanf("%s%d", op, &x);
+
+        if (*op == 'I') insert(x);
+        else
+        {
+            if (find(x)) puts("Yes");
+            else puts("No");
+        }
+    }
+
+    return 0;
+}
+
