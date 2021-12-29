b6036c2ceffdcd267fc612994bb011cf61c11d89
030666 <2028800763@qq.com>
2021-12-29 22:02:01 +0800

    字符串哈希

--- 2.数据结构/AcWing840模拟散列表(哈希表-开放寻址法)cpp.cpp
+++ 2.数据结构/AcWing840模拟散列表(哈希表-开放寻址法)cpp.cpp
@@ -1,49 +1,33 @@
-// 时间：2021.12.28 22点48分 
-//	开放寻址法 
-#include <cstring>
-#include <iostream>
-
+#include<iostream>
 using namespace std;
-
-const int N = 200003,null=0x3f3f3f3f;//取模的这个数N最好是在2n~3n之间的质数，这样出现哈希冲突的概率最小
-
-int h[N];
  
-
-int find(int x)
+typedef unsigned long long ULL;
+ 
+const int N=100010,p=131;
+ 
+int n,m;
+char str[N];
+ULL h[N],p[N];//p数组用来存储p的n次方
+ 
+ULL get(int l,int r)
 {
-    int k = (x % N + N) % N;
-    while(h[k]!=null && h[k]!=x)//如果当前这个坑位有人而且不是x自己，则扫描下一个 
-    {
-    	k++;
-    	if(k==N) k=0;//如果扫描完了，则重新开始 
-	}
-  return k;//这里的k有两层意思，一是x存在的位置，二是k应该存在的位置 
+     return h[r]-h[l-1]*p[r-l+1];
 }
-
-int main()
-{
-    int n;
-    scanf("%d", &n);
-
-    memset(h, 0x3f, sizeof h);
-//memset函数是按字节赋值，一个数四个字节，一个字节0x3f那么一个数就是0x3f3f3f3f 
-    while (n -- )
-    {
-        char op[2];
-        int x;
-        scanf("%s%d", op, &x);
-	
-		int k=find(x);
-        if (*op == 'I') 
-        	h[k]=x;
-        else
-        {
-            if (h[k]!=null) puts("Yes");
-            else puts("No");
-        }
-    }
-
-    return 0;
-}
-
+ int main ()
+ {
+     scanf("%d%d%s",&n,&m,str+1);
+     p[0]=1;
+     for(int i=1;i<=n;i++)
+     {
+            p[i]=p[i-1]*p;
+            h[i]=h[i-1]*p+str[i];
+     }
+     while(m--)
+     {
+         int l1,r1,l2,r2;
+         scanf("%d%d%d%d",&l1,&r1,&l2,&r2);
+         if(get(l1,l2)==get(l2,r2)) puts("Yes");
+         else puts("No");
+     }
+     return 0;
+ }
