369a2af3ab774b34e8e7b6b1969017fa9f96d15b
030666 <2028800763@qq.com>
2022-01-01 15:24:59 +0800

    哈希

--- 2.数据结构/AcWing139.回文子串的最大长度(字符串哈希.二分).cpp
+++ 2.数据结构/AcWing139.回文子串的最大长度(字符串哈希.二分).cpp
@@ -1,63 +1,38 @@
-// 时间：2021.12.31 16点37分
-// 算法：字符串哈希 二分 
- 
-//回文数可能是奇数个也可能是偶数个，我们可以在每两个数之间插入一个没出现过的字符
-//来将回文数统一成奇数个,还不会对结果产生影响
-
-//本题做题思路：1、枚举中点  2、二分半径
+// 时间：2022.1.1 13点52分
+// 算法：DFS 
 
 #include<iostream>
-#include<string.h>
-#include<algorithm>
 using namespace std;
 
-typedef unsigned long long ULL;
-const int N=2000010,base=131;
+const int N=10;
+int n;
+int path[N];
+bool st[N];//为true表示这个数被用过了
 
-char str[N];
-ULL hl[N],hr[N],p[N];
-
-ULL get(ULL h[],int l,int r)
+void dfs(int u)
 {
-    return h[r]-h[l-1]*p[r-l+1];
+    if(u==n)//u代表递归的层数，当U等于n时说明已经到达叶节点
+    {
+        for(int i=0;i<n;i++) printf("%d ",path[i]);
+        puts(" ");
+        return;
+    }
+     for(int i=1;i<=n;i++)
+        if(!st[i])//如果i没有用过
+        {
+            path[u]=i;
+            st[i]=true;
+            dfs(u+1);
+            //path[u]=0;
+            st[i]=false;
+        }
 }
 
 int main ()
 {
-    int T=1;
-    while(scanf("%s",str+1),strcmp(str+1,"END"))
-    {
-        int n=strlen(str+1);
-        //插入字符
-        for(int i=n*2;i>0;i-=2)
-        {
-            str[i]=str[i/2];
-            str[i-1]='z'+1;
-        }
-        n*=2;
-        
-        p[0]=1;
-        for(int i=1,j=n;i<=n;i++,j--)//i用来取前缀哈希值，j用来取后缀哈希值
-        {
-            hl[i]=hl[i-1]*base+str[i]-'a'+1;
-            hr[i]=hr[i-1]*base+str[j]-'a'+1;
-            p[i]=p[i-1]*base;
-        }
-        //枚举中点，二分半径
-        int res=0;
-        for(int i=1;i<=n;i++)
-        {
-            int l=0,r=min(i-1,n-i);
-            while(l<r)
-            {
-                int mid=l+r+1>>1;
-                if(get(hl,i-mid,i-1)!=get(hr,n-(i+mid)+1,n-(i+1)+1)) r=mid-1;//范围太大了，需要缩小
-                else l=mid;
-            }
-            if(str[i-l]<='z') res=max(res,l+1);
-            else res=max(res,l);
-        }
-        printf("Case %d: %d\n",T++,res);
-    }
+    cin>>n;
+    
+    dfs(0);
+    
     return 0;
 }
