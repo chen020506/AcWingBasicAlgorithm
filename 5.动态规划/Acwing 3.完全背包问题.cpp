a17e7596cf587c5bbc8090c5ad9cf898a233c766
030666 <2028800763@qq.com>
2022-06-09 20:55:50 +0800

    动态规划之完全背包问题

--- 5.动态规划/Acwing 3.完全背包问题.cpp
+++ 5.动态规划/Acwing 3.完全背包问题.cpp
@@ -1,4 +1,4 @@
-//  时间：2022.06.08 21点57分
+//  时间：2022.06.09  20点55分
 #include<iostream>
 #include<algorithm>
 using namespace std;
@@ -7,7 +7,39 @@ const int N=1010;
 int n,m;
 int v[N],w[N];
 int f[N][N];
-// 第二种：朴素做法，现在这个代码在Acwing已经不能ac了，会TLE
+
+//第一种：优化plus版（一维）,注意这种方法需要将f数组变成一维
+// int main ()
+// {
+//     cin>>n>>m;
+//     for(int i=1;i<=n;i++) cin>>v[i]>>w[i];
+    
+//     for(int i=1;i<=n;i++)
+//         for(int j=v[i];j<=m;j++)
+//             {
+//              f[j]=max(f[j],f[j-v[i]]+w[i]);
+//             }
+//     cout<<f[m]<<endl;
+//     return 0;
+// }
+
+// 第二种：优化版（二维） 将三重循环变为了双重循环
+int main ()
+{
+    cin>>n>>m;
+    for(int i=1;i<=n;i++) cin>>v[i]>>w[i];
+    
+    for(int i=1;i<=n;i++)
+        for(int j=0;j<=m;j++)
+            {
+                f[i][j]=f[i-1][j];
+                if(j>=v[i]) f[i][j]=max(f[i][j],f[i][j-v[i]]+w[i]);
+            }
+    cout<<f[n][m]<<endl;
+    return 0;
+}
+
+// 第三种：朴素做法（二维），有三重循环，现在这个代码在Acwing已经不能ac了，会TLE
 // int main ()
 // {
 //     cin>>n>>m;
